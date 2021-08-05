6cdb90defef78e420d6b63eaeb5e51978418f00d
git_user_name_here <git_user@email.here>
2021-08-05 15:53:21 +0300

    feat: add JWT auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { OwnerModule } from 'src/owner/owner.module';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { localStrategy } from './local.strategy';
 
 @Module({
-  imports: [OwnerModule, PassportModule],
-  providers: [AuthService, localStrategy]
+
+  imports: [
+    ConfigModule.forRoot(),
+    OwnerModule,
+    PassportModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' }
+    })],
+  providers: [AuthService, localStrategy, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { OwnerService } from 'src/owner/owner.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private ownerService: OwnerService) { }
+    constructor(
+        private ownerService: OwnerService,
+        private jwtService: JwtService
+    ) { }
 
     async validateUser(name: string, password: string): Promise<any> {
         const user = await this.ownerService.findOne(name);
-        return user
+        if (user && user.password === password) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async login(user: any) {
+        const payload = { username: user.username, sub: user.id }
+        return {
+            access_token: this.jwtService.sign(payload)
+        }
     }
 }
